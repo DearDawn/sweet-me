3e9f643e8ce3648de329b444a15f566234a3bc86
tangqiuqiu <tangqiuqiu@pinduoduo.com>
2024-01-09 17:43:19 +0800

    feat: 更新类型引用位置

--- src/common/button/index.tsx
+++ src/common/button/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import cs from 'clsx';
 import * as styles from './index.module.less'
-import { ICommonProps } from 'src/types';
+import { ICommonProps } from '../../types';
 
 interface IProps extends ICommonProps {
   /** 按钮大小 */

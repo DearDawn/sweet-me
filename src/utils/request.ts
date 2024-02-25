852225c105bc65bf3caca166d1c4f37b8b7f0b30
tangqiuqiu <dawn.tang@temu.com>
2024-02-25 11:33:41 +0800

    feat: post

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,3 +6,10 @@ export const apiGet = <T> (input: RequestUrl, params: Record<string, any> = {}, 
 
   return fetch(url, init).then(res => res.json()) as Promise<T>;
 };
+
+export const apiPost = <T> (input: RequestUrl, params: Record<string, any> = {}, body: Record<string, any> = {}, init?: RequestInit) => {
+  const searchStr = new URLSearchParams({ ...params }).toString();
+  const url = searchStr ? `${input}?${searchStr}` : input;
+
+  return fetch(url, { method: 'POST', body: JSON.stringify(body), ...init }).then(res => res.json()) as Promise<T>;
+};

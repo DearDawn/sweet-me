c0e6ea773ae2bf725ef62e55339ba8361cb797f8
tangqiuqiu <dawn.tang@temu.com>
2025-01-17 16:18:43 +0800

    feat: update

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './global.less';
 import { App } from './App';
+export const holder = {};
 
 const app = document.getElementById('app')!;
 const root = createRoot(app);
+
 root.render(<App />);

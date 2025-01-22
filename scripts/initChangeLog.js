af0c629848a8a6de2ab0d2456c5d1d7282b6ba53
tangqiuqiu <dawn.tang@temu.com>
2025-01-22 22:30:18 +0800

    feat: add icon

--- scripts/initChangeLog.js
+++ scripts/initChangeLog.js
@@ -13,7 +13,7 @@ function gitCommit (noUpdate = false) {
   try {
     execSync('git add CHANGE_LOG.md'); // 添加 CHANGELOG.md 到暂存区
     if (!noUpdate) {
-      execSync(`git commit -m "v${version} CHANGE_LOG" --amend"`); // 提交更改
+      execSync(`git commit -m "v${version} CHANGE_LOG" --amend`); // 提交更改
     }
     console.log('CHANGE_LOG 更新已提交到 Git 仓库');
     execSync('git push'); // 提交更改

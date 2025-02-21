8f42462b2bfc751e3c3f292376bab8d9d5d456a7
tangqiuqiu <dawn.tang@temu.com>
2025-02-21 16:58:38 +0800

    feat: update

--- src/common/space/index.tsx
+++ src/common/space/index.tsx
@@ -5,14 +5,14 @@ import { ICommonProps } from '../../types';
 
 type IProps = ICommonProps & {
   /** 间距 */
-  gap?: string
+  gap?: string;
   /** 内边距 */
-  padding?: string
+  padding?: string;
   /** 是否为竖直排列 */
   isColumn?: boolean;
   /** 是否拉伸 */
   stretch?: boolean;
-}
+};
 
 /** 间距容器 */
 export const Space = ({
@@ -22,12 +22,13 @@ export const Space = ({
   padding = '10px',
   isColumn = false,
   stretch = false,
+  style,
   ...rest
 }: IProps & React.HTMLAttributes<HTMLDivElement>) => {
 
   return (
     <div
-      style={{ gap, padding }}
+      style={{ gap, padding, ...style }}
       className={cs(styles.space, className, {
         [styles.column]: isColumn,
         [styles.stretch]: stretch
